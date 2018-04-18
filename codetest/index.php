b4b2ae2577f34fc62bd1ad97494482677d485a38
Ravindra Kumar Singh <ravindrakumar.singh@iopex.com>
2018-04-19 02:17:10 +0530

    Released first version

--- codetest/index.php
+++ codetest/index.php
@@ -1,9 +1,79 @@
 <?php
+
 require 'tests/index.php';
 
+/**
+ * Function will build necklace
+ * based on given parameter
+ * 
+ * @param int $blue
+ * @param int $red
+ * @param int $yellow
+ * @param int $green
+ * @return int Number of used ruby
+ */
 function makeNecklace($blue, $red, $yellow, $green)
 {
-    $output = 0;
+    $num = 0;
+    $prev = 0;
+    $limit = 2000;
+    
+    for ($i = 0; $i < $limit; $i++)
+    {
+        if ($prev == 0 || $prev == 2)
+        {
+            if($blue)
+            {
+                $blue--;
+                $num++;
+                $prev = 0;
+                draw('blue');
+            }
+            else if($red)
+            {
+                $red--;
+                $num++;
+                $prev = 1;
+                draw('red');
+            }
+        }
+        else if($prev == 1 || $prev == 3)
+        {
+            if($green)
+            {
+                $green--;
+                $num++;
+                $prev = 3;
+                draw('green');
+            }
+            else if($yellow)
+            {
+                $yellow--;
+                $num++;
+                $prev = 2;
+                draw('yellow');
+            }
+        }
+    }
+    drawLine($num);
+    return $num;
+}
 
-    return $output;
+/**
+ * Draw val
+ * @param type $val
+ */
+function draw($val)
+{
+    echo '<span style="display:inline-block;width:25px;height:25px;border-radius:50%;background:'.$val.'">&nbsp;</span>';
+}
+
+/**
+ * Function draw line
+ * @param none
+ * @return void 
+ */
+function drawLine($num)
+{
+    if($num) echo '<hr/>';
 }
--- codetest/tests/index.php
+++ codetest/tests/index.php
@@ -18,15 +18,33 @@ function tests()
             "expect" => 990,
             "msg" => "Should work with 989 blue, 147 red,  0 yellow, and 0 green"
         ),
+        array(
+            "input" => [0,10,0,0],
+            "expect" => 1,
+            "msg" => "Should work with 0 blue, 10 red,  0 yellow, and 0 green"
+        ),
+        array(
+            "input" => [0,0,5,0],
+            "expect" => 0,
+            "msg" => "Should work with 0 blue, 0 red,  5 yellow, and 0 green"
+        ),
     );
     
+    $totalTest = 0;
+    $passed = 0;
     foreach($testCase as $key => $value)
     {
+        $totalTest++;
         $output = call_user_func_array("makeNecklace", $value['input']);
         if($value['expect'] !== $output)
         {
-            echo 'Expected: '.$value['expect'].' : '.$output.'<h5 style="color:red;">Failed case '.($key+1).': '.$value['msg'].'</h5>';
+            echo 'Expected: '.$value['expect'].' : '.$output.'<pre style="color:red;">Failed case '.($key+1).': '.$value['msg'].'</pre>';
+        }
+        else
+        {
+            $passed++;
         }
     }
+    echo '<hr/><pre>Passed ('.$passed.'/'.$totalTest.') = '.number_format($passed*100/$totalTest,2).'%</pre>';
 }
 tests();
