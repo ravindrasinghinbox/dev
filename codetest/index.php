f3ac3ac8c3edaf9b6ec17c8d17c5ed3d01c9472f
Ravindra Kumar Singh <ravindrakumar.singh@iopex.com>
2018-04-21 18:11:00 +0530

    Created new test and optimized code

--- codetest/index.php
+++ codetest/index.php
@@ -1,73 +1,60 @@
 <?php
 
-require 'tests/index.php';
+require 'tests/indexTest.php';
 
-    /**
-     * Function will build necklace
-     * based on given parameter
-     * 
-     * @param int $blue
-     * @param int $red
-     * @param int $yellow
-     * @param int $green
-     * @return int Number of used ruby
-     */
-    function makeNecklace($blue, $red, $yellow, $green)
+/**
+ * Function will build necklace
+ * based on given parameter
+ * 
+ * @param int $blue
+ * @param int $red
+ * @param int $yellow
+ * @param int $green
+ * @return int Number of used ruby
+ */
+function makeNecklace($blue = 0, $red = 0, $yellow = 0, $green = 0)
+{
+    $limit = $blue + $red + $yellow + $green;
+    $count = 0;
+    $prev = NULL;
+
+    for ($i = 0; $i <= $limit; $i++)
     {
-        $num = 0;
-        $prev = null;
-        $limit = $blue+$red+$yellow+$green;
-
-        for ($i = 0; $i < $limit; $i++)
+        $index = $i % 4;
+        if (($prev === NULL && $blue) || ($prev == 0 && $blue) || ($prev == 2 && $blue))
         {
-            if(($prev == null || $prev == 0 || $prev == 2) && $blue)
-            {
-                $blue--;
-                $num++;
-                $prev = 0;
-                draw('blue');
-            }
-            else if(($prev == null || $prev == 2 || $prev == 0) && $red)
-            {
-                $red--;
-                $num++;
-                $prev = 1;
-                draw('red');
-            }
-            else if(($prev == null || $prev == 1 || $prev == 3) && $green)
-            {
-                $green--;
-                $num++;
-                $prev = 3;
-                draw('green');
-            }
-            else if(($prev == null || $prev == 3 || $prev == 1) && $yellow)
-            {
-                $yellow--;
-                $num++;
-                $prev = 2;
-                draw('yellow');
-            }
+            $blue--;
+            draw('blue');
+            $prev = 0;
         }
-        drawLine($num);
-        return $num;
+        elseif (($prev === NULL && $red) || ($prev == 0 && $red) || ($prev == 2 && $red))
+        {
+            $red--;
+            draw('red');
+            $prev = 1;
+        }
+        elseif (($prev === NULL && $green) || ($prev == 1 && $green) || ($prev == 3 && $green))
+        {
+            $green--;
+            draw('green');
+            $prev = 3;
+        }
+        elseif (($prev === NULL && $yellow) || ($prev == 1 && $yellow) || ($prev == 3 && $yellow))
+        {
+            $yellow--;
+            draw('yellow');
+            $prev = 2;
+        }
     }
+    return $limit - ($blue + $red + $yellow + $green);
+}
 
 /**
  * Draw val
  * @param type $val
  */
-function draw($val)
+function draw($val = '')
 {
-    echo '<span style="display:inline-block;width:25px;height:25px;border-radius:50%;line-height: 24px;text-align:center;background:'.$val.'">'.substr($val,0,1).'</span>';
+    echo '<span style="display:inline-block;width:25px;height:25px;border-radius:50%;line-height: 24px;text-align:center;background:'.($val=='green'?'limegreen':$val).'">'.substr($val,0,1).'</span>';
 }
 
-/**
- * Function draw line
- * @param none
- * @return void 
- */
-function drawLine($num)
-{
-    if($num) echo '<hr/>';
-}
---
+++ codetest/tests/Summary.php
@@ -0,0 +1,76 @@
+<?php
+class Summary{
+    
+    var $startTime;
+    var $time;
+    var $totalTest;
+    var $passedTest;
+    var $timeUnit = 'Sec';
+    var $memory;
+    var $memoryUnit = 'KB';
+    var $memoryUnits = array(
+        "BYTE" => 1,
+        "KB" => 1024,
+        "MB" => 1024*1024
+    );
+    
+    public function __construct()
+    {
+        $this->startTime = $this->getMicroTime();
+        $this->memory = memory_get_peak_usage(TRUE);
+    }
+    
+    private function endProfile()
+    {
+        $this->time = number_format($this->getMicroTime() - $this->startTime,3,'.','');
+        $this->memory = intval((memory_get_peak_usage(TRUE) - $this->memory)/$this->memoryUnits[$this->memoryUnit]);
+    }
+    
+    public function report($data = array())
+    {
+        $this->endProfile();
+        $this->totalTest++;
+        if($data['expect'] === $data['output'])
+        {
+            $this->passedTest++;
+            echo '<br/><br/>';
+        }
+        else
+        {
+            $output = '<table width="500" border="1" cellpadding="1" cellspacing="1">'
+                    . '<thead>'
+                        . '<tr><th colspan="2" style="background:red;color:yellow;">'.$data['msg'].'</th></tr>'
+                    . '</thead>'
+                    . '<tbody>'
+                        . '<tr><td>Id</td><td>'.$data['id'].'</td></tr>'
+                        . '<tr><td>Time</td><td>'.$this->time.' ('.$this->timeUnit.')</td></tr>'
+                        . '<tr><td>Memory</td><td>'.$this->memory.' ('.$this->memoryUnit.')</td></tr>'
+                        . '<tr><td>Expected ('.$data['expect'].')</td><td style="color:red;">'.$data['output'].'</td></tr>'
+                        . '<tr><td>Status </td><td style="color:red;">Failed</td></tr>'
+                    . '</tbody>'
+                    . '</table>';
+            echo $output;
+        }
+    }
+    
+    private function getMicroTime($float = TRUE)
+    {
+        
+        return number_format(microtime($float),3,'.','');
+    }
+    
+    public function __destruct()
+    {
+        $progress = number_format($this->passedTest*100/$this->totalTest,2);
+        $output = '<table width="500" border="1" cellpadding="1" cellspacing="1">'
+                . '<thead>'
+                    . '<tr><th colspan="2" style="background:coral;color:white;">Summary</th></tr>'
+                . '</thead>'
+                . '<tbody>'
+                    . '<tr><td>Test Passed ('.$this->passedTest.'/'.$this->totalTest.') </td><td><div style="text-align:center;width:'.$progress.'%;background:limegreen;color:white;">'.$progress.'%</div></td></tr>'
+                    . '<tr><td>Status</td><td>'.($this->passedTest == $this->totalTest ?'Success':'Failed').'</td></tr>'
+                . '</tbody>'
+                . '</table>';
+        echo $output;
+    }
+}
