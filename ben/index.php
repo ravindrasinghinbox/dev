4efeff8d09d4770b27eba8b45aa06cb01d185a59
Ravindra Singh <ravindrasinghinbox@gmail.com>
2018-04-30 02:03:50 +0530

    Added initial code

--- ben/index.php
+++ ben/index.php
@@ -1,65 +1,36 @@
 <?php
 
-require 'tests/indexTest.php';
+//require 'tests/indexTest.php';
 
 /**
- * Function will build necklace
+ * Get minimum game cost
  * based on given parameter
  * 
- * @param int $blue
- * @param int $red
- * @param int $yellow
- * @param int $green
- * @return int Number of used ruby
+ * @param int $level and $weapon separated by space
+ * @param int nth level of element
+ * @return int minimum cost for game
  */
-function makeNecklace($blue = 0, $red = 0, $yellow = 0, $green = 0)
-{
-    $limit = $blue + $red + $yellow + $green;
-    $prev = NULL;
+function gameCost($levelAndWeapon, $lines = array()) {
+    // Sort element
+    sort($lines);
+    // Extract type and weapon
+    $levelAndWeapon = explode(' ', $levelAndWeapon);
+    $level = $levelAndWeapon[0];
+    $weapon = $levelAndWeapon[1];
+    // Unset unuse variable
+    unset($levelAndWeapon);
 
-    while (TRUE)
-    {
-        if ($blue && ($prev === NULL || $prev == 0 || $prev == 2))
-        {
-            $blue--;
-            $prev = 0;
-        }
-        elseif ($red && ($prev === NULL || $prev == 0 || $prev == 2))
-        {
-            $red--;
-            $prev = 1;
-        }
-        elseif ($green && ($prev === NULL || $prev == 1 || $prev == 3))
-        {
-            $green--;
-            $prev = 3;
-        }
-        elseif ($yellow && ($prev === NULL || $prev == 1 || $prev == 3))
-        {
-            $yellow--;
-            $prev = 2;
-        }
-        else
-        {
+    $coins = 0;
+    foreach ($lines as $key => $value) {
+        $num = substr_count($value, '1');
+        $coins += $coins?$num:($num*$num);
+        if ($coins + $num > $level)
             break;
-        }
-        draw($prev);
     }
-    return $limit - ($blue + $red + $yellow + $green);
+    echo $coins;
 }
 
-/**
- * Draw val
- * @param type $val
- */
-function draw($val = '')
-{
-    $colors = array(
-        0 => 'blue',
-        1 => 'red',
-        2 => 'yellow',
-        3 => 'green'
-    );
-    echo '<span style="display:inline-block;width:25px;height:25px;border-radius:50%;line-height: 24px;text-align:center;background:'.($colors[$val]=='green'?'limegreen':$colors[$val]).'">'.substr($colors[$val],0,1).'</span>';
-}
-
+$lines = ['111', '101', '010']; $levelAndWeapon = '3 3';
+//$lines = ['0101'];  $levelAndWeapon = '1 4';
+echo '<pre>';
+gameCost($levelAndWeapon, $lines);
