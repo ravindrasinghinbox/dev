ad3140ac84402fc3ff393e251f7a09c7edcf01a6
Ravindra Singh <ravindrasinghinbox@gmail.com>
2018-05-11 00:13:54 +0530

    Added game

--- bob-the-bear/index.php
+++ bob-the-bear/index.php
@@ -1,8 +1,23 @@
 <?php
-
 require 'tests/indexTest.php';
 
-function cacheMaximumFishes($input = array())
-{
+function cacheMaximumFishes($input = array()){
+
+    // Getting input parameter
+    $size = $input['size'];
+    $len = $input['len'];
+    $time = $input['time'];
+
+    // Variable initialization goes here
     $fishCount = 0;
+
+
+    var_dump('before: ',$time);
+    $time = getUniqueTime($time);
+    var_dump('after: ',$time);
+}
+
+function getUniqueTime(){
+    
+    $arr = array_unique($arr);
 }
