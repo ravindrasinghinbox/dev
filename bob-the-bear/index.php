3debeac24e0b0567f8f3377db85e4c1bfc1b6f05
Ravindra Singh <ravindrasinghinbox@gmail.com>
2018-05-12 02:41:12 +0530

    Completed bob-the-bear

--- bob-the-bear/index.php
+++ bob-the-bear/index.php
@@ -5,19 +5,27 @@ function cacheMaximumFishes($input = array()){
 
     // Getting input parameter
     $size = $input['size'];
-    $len = $input['len'];
-    $time = $input['time'];
+    $lens = explode(' ',$input['len']);
+    $times = explode(' ',$input['time']);
+    $timesLen = count($times);
 
     // Variable initialization goes here
     $fishCount = 0;
 
-
-    var_dump('before: ',$time);
-    $time = getUniqueTime($time);
-    var_dump('after: ',$time);
-}
-
-function getUniqueTime(){
+    // Get unique sorted time 
+    $uniqueTimes = array_unique($times);
+    sort($uniqueTimes);
+    $uniqueTimesLen = count($uniqueTimes);
     
-    $arr = array_unique($arr);
+    for($i = 0; $i < $uniqueTimesLen; $i++){
+        for($j = 0; $j < $timesLen; $j++){
+            if($uniqueTimes[$i] == $times[$j]){
+                echo ($j+1).',';
+            }
+            else if($uniqueTimes[$i] < $lens[$j]+$times[$j] && $uniqueTimes[$i] > $times[$j]){
+                echo ($j+1).',';
+            }
+        }
+        echo '<br/>';
+    }
 }
