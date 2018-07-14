eeb8429a5e92659df299a9c91779ba59fdeaa696
Ravindra Singh <ravindrasinghinbox@gmail.com>
2018-07-15 03:15:13 +0530

    Added zombie test

--- aimbooster/js/main.js
+++ aimbooster/js/main.js
@@ -6,45 +6,50 @@ function App(config) {
     _this = {};
     config = (typeof (config) == 'object') ? config : {};
     _this.config = Object.assign({
-        canvasId: 'myCanvas',
-        imgId: 'myImg',
+        canvasId: 'canvas',
+        imgId: 'bg',
+        shootId: 'shoot',
+        zombieId: 'zombie',
         canvasHeight: window.innerHeight,
         canvasWidth: window.innerWidth,
     }, config);
     $canvas = document.getElementById(_this.config.canvasId);
     $img = document.getElementById(_this.config.imgId);
+    $zombie = document.getElementById(_this.config.zombieId);
+    $shoot = document.getElementById(_this.config.shootId);
 
     /**
      * Start application
      * 
      */
-    this.play = function(){
+    this.play = function () {
         setViewport();
         drawBackground();
+        drawZombie();
     }
 
     /**
      * Pause application
      */
-    this.pause = function(){
+    this.pause = function () {
 
     }
     /**
      * Stop application
      */
-    this.stop = function(){
+    this.stop = function () {
 
     }
-    
+
     /**
      * Draw background
      * 
      */
     drawBackground = function () {
         var ctx = $canvas.getContext("2d");
-        ctx.drawImage($img, 
-            0, 0,$img.width,$img.height,
-            0, 0,_this.config.canvasWidth,_this.config.canvasHeight,
+        ctx.drawImage($img,
+            0, 0, $img.width, $img.height,
+            0, 0, _this.config.canvasWidth, _this.config.canvasHeight,
         );
     }
 
@@ -54,9 +59,10 @@ function App(config) {
      */
     drawZombie = function () {
         var ctx = $canvas.getContext("2d");
-        ctx.drawImage($img, 
-            0, 0,$img.width,$img.height,
-            0, 0,_this.config.canvasWidth,_this.config.canvasHeight,
+        ctx.drawImage($zombie,
+            0, 0, $zombie.width, $zombie.height,
+            0, 0,
+            _this.config.canvasWidth, _this.config.canvasHeight,
         );
     }
 
@@ -66,10 +72,18 @@ function App(config) {
      * @param {integer} height height of canvas
      * @param {integer} width width of canvas
      */
-    setViewport = function (height,width) {
+    setViewport = function (height, width) {
         height = height || _this.config.canvasHeight;
         width = width || _this.config.canvasWidth;
         $canvas.height = height;
         $canvas.width = width;
+    }
+
+    /**
+     * Shoot gun
+     */
+    this.shoot = function () {
+        $shoot.currentTime = 0;
+        $shoot.play();
     }
 }
