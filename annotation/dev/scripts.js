994e4b9c29b9f92d6a7f4567a629afc6c50486c2
Ravindra Singh <ravindrasinghinbox@gmail.com>
2018-10-26 16:40:17 +0530

    Fixed annotation

--- annotation/dev/scripts.js
+++ annotation/dev/scripts.js
@@ -3007,8 +3007,8 @@ function _via_update_ui_components() {
 /**
  * Prevent keyboard assign for text editor
  */
-function preventEditor(e) { return false;
-  if (e.target.nodeName !== 'BODY') {
+function preventEditor(e) { 
+  if (e.target.nodeName == 'TEXTAREA') {
     return true;
   }
   else {
@@ -4034,22 +4034,7 @@ function updateLayerCheckbox(index, status) {
   elem[index].checked = status;
   return status;
 }
-function dataURItoBlob(dataURI) {
-  // convert base64/URLEncoded data component to raw binary data held in a string
-  var byteString;
-  if (dataURI.split(',')[0].indexOf('base64') >= 0)
-      byteString = atob(dataURI.split(',')[1]);
-  else
-      byteString = unescape(dataURI.split(',')[1]);
-  // separate out the mime component
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-  // write the bytes of the string to a typed array
-  var ia = new Uint8Array(byteString.length);
-  for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-  }
-  return new Blob([ia], {type:mimeString});
-}
+
 //
 // hooks for sub-modules
 // implemented by sub-modules
